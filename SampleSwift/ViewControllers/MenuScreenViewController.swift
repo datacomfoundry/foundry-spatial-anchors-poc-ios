4447a157bd05aafe4c6970d2ca65061b4b5258cd
Dipesh Trikam <dipesht@datacom.co.nz>
2021-06-27 17:57:56 +1200

    Added both scene files into project

--- SampleSwift/ViewControllers/MenuScreenViewController.swift
+++ SampleSwift/ViewControllers/MenuScreenViewController.swift
@@ -14,6 +14,7 @@ import UIKit
 class MenuScreenViewController: UIViewController, SCNSceneRendererDelegate {
 
     @IBOutlet weak var scenePreviewView1: SCNView!
+    @IBOutlet weak var scenePreviewView2: SCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +22,26 @@ class MenuScreenViewController: UIViewController, SCNSceneRendererDelegate {
         scenePreviewView1.delegate = self
         scenePreviewView1.autoenablesDefaultLighting = true
         scenePreviewView1.allowsCameraControl = true
-        scenePreviewView1.scene = self.setupScene()
+        scenePreviewView1.scene = self.setupScene1()
+
+        scenePreviewView2.delegate = self
+        scenePreviewView2.autoenablesDefaultLighting = true
+        scenePreviewView2.allowsCameraControl = true
+        scenePreviewView2.scene = self.setupScene2()
 
     }
 
-    func setupScene() -> SCNScene? {
+    func setupScene1() -> SCNScene? {
 
         guard let scene = SCNScene(named: "art.scnassets/toy_biplane.usdz")
+        else { return nil }
+
+        return scene
+    }
+
+    func setupScene2() -> SCNScene? {
+
+        guard let scene = SCNScene(named: "art.scnassets/ship.scn")
         else { return nil }
 
         return scene
